85ebd52ae8feb76f516c0a0896b082c980ecebe1
Diego Moreira <diegorm.ti@gmail.com>
2022-12-02 21:53:12 -0300

    Create project for new login CRT4

---
+++ src/app/core/authentication/teclado-virtual/teclado-virtual.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teclado-virtual',
+  templateUrl: './teclado-virtual.component.html',
+  styleUrls: ['./teclado-virtual.component.scss']
+})
+export class TecladoVirtualComponent {
+
+}
